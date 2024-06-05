ab9c810ef1b0179f9b9461f883b79ee7a781f2c8
Sagar Gowda <46260031+sagargowda88@users.noreply.github.com>
2024-06-05 15:13:48 +0530

    Create config_change.py

---
+++ config_change.py
@@ -0,0 +1,17 @@
+{
+    "mlflow": {
+        "tracking_uri": "<mlflow_tracking_uri>",
+        "username": "<mlflow_username>",
+        "password": "<mlflow_password>"
+    },
+    "aws": {
+        "access_key_id": "<aws_access_key_id>",
+        "secret_access_key": "<aws_secret_access_key>",
+        "s3_endpoint_url": "<mlflow_s3_endpoint_url>"
+    },
+    "model_uris": [
+        "model_uri=f\"models:/roberta/1\"",
+        "model_uri=f\"models:/bert/2\"",
+        "model_uri=f\"models:/gpt/3\""
+    ]
+}
